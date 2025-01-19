eb31b137d3b3298b073721b956ea025224b435ad
Sean Nie <81334381+ImSe4n@users.noreply.github.com>
2025-01-18 23:43:18 -0500

    backend - jobs + get all jobs

--- backend/routes/jobs.js
+++ backend/routes/jobs.js
@@ -38,4 +38,15 @@ router.post('/post-job', async (req, res) => {
     });
 });
 
+router.get('/get-all-jobs', async (req, res) => {
+    let jobs = await collection.find().toArray();
+    if (jobs.length === 0) {
+        return res.send('No jobs found');
+    }
+
+    // remove jobs with requested skill not equal to 'Technology'
+    jobs = jobs.filter((job) => job.requestedSkill === 'Technology');
+    return res.json(jobs);
+});
+
 module.exports = router;
