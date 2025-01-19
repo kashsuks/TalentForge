b3c830ab56c5a5fecccbc135539ed5839c1edfa2
Kashyap Sukshavasi <ksukshavasi@gmail.com>
2025-01-19 00:46:05 -0500

    main page

---
+++ frontend/skill-sharing-app/src/Components/Hero.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Hero.css';
+
+function Hero() {
+  const navigate = useNavigate();
+
+  const handleRequestSkillClick = () => {
+    navigate('/request-skill');
+  };
+
+  return (
+    <section className="hero">
+      <h1 className="hero-slogan">Share.<br /> Ask.<br /> Gain Talent.</h1>
+      <div className="hero-button">
+        <button className="request-skill-button" onClick={handleRequestSkillClick}>Request a Skill</button>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
